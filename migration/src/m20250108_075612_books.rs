0eadb810766c6db687eb3d191c01e77e46f02628
ghost-str <Ghost-str@users.noreply.github.com>
2025-01-08 11:11:48 +0300

    error example

---
+++ migration/src/m20250108_075612_books.rs
@@ -0,0 +1,32 @@
+use loco_rs::schema::table_auto_tz;
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                table_auto_tz(Books::Table)
+                    .col(uuid(Books::Id).primary_key())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Books::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Books {
+    Table,
+    Id,
+    
+}
+
